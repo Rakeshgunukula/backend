2430b36e5acb48b33128fc1ee9d753d2d07051e5
Gunukula Rakesh <gunukularakesh7@gmail.com>
2026-02-08 09:54:25 +0530

    commit changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const routes = require('./routes/routes');
 const app = express()
 app.use(cors(
     {
-        origin:'https://frontend-blond-ten-4yhr83pw4k.vercel.app/',
+        origin:'https://frontend-deusya4ek-gunukula-rakeshs-projects.vercel.app/',
         methods: ['GET','POST','DELETE'],
         credentials: true,
     }

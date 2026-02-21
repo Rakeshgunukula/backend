8f05b2f858359093301e83658f37f95c67e41f24
Gunukula Rakesh <gunukularakesh7@gmail.com>
2026-02-21 23:40:22 +0530

    commit changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     // ===========*Conneting localhost server to mongodb *============
 
 const express = require('express')
-
+const path = require('path');
 const mongoose = require('mongoose')
 
 require('dotenv').config()
@@ -16,7 +16,7 @@ app.use(cors());
 
     app.use(express.json())
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../frontend')))
 
 app.use(express.urlencoded({extended:true}));
 
